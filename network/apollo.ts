778d79c4a7f2d8477609f243da0f28b8e384e1cd
jiiin <jiiin.cho.dev@gmail.com>
2022-06-19 21:35:10 +0200

    Apollo cache setup updated, landing does not revalidate when deployed

--- network/apollo.ts
+++ network/apollo.ts
@@ -3,6 +3,16 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://fika-2022.herokuapp.com/",
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
+    },
+  },
 });
 
 export default client;
